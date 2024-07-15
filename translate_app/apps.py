ce7b9db1f1367c71295f44f852176aebc0d51b3b
Navaneeth1510 <117336493+Navaneeth1510@users.noreply.github.com>
2024-07-15 10:04:07 +0530

    add files via upload

--- translate_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TranslatorAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "translator_app"
--- translate_app/urls.py
+++
@@ -1,7 +0,0 @@
-# translate_app/urls.py
-from django.urls import path
-from .views import translate_text
-
-urlpatterns = [
-    path('translate/', translate_text, name='translate'),
-]
